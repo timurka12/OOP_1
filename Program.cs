d769ccc728fae5decd1a51fc2425e05647c3de58
timurka12 <93543993+timurka12@users.noreply.github.com>
2022-03-03 00:40:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    abstract class Person
+    {
+        abstract public void Live();
+    }
+
+    class worker : Person
+    {
+        public String nameofcompany { get; set; }
+        public override void Live()
+        {
+            Console.WriteLine("worker work in " + nameofcompany);
+        }
+    }
+
+    class philanthropist : Person
+    {
+        public String fonds { get; set; }
+        override public void Live()
+        {
+            Console.WriteLine("philanthropist help " + fonds);
+        }
+    }
+
+    class business : Person
+    {
+        public string restaurant { get; set; }
+        override public void Live()
+        {
+            Console.WriteLine("He has a business - " + restaurant);
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
